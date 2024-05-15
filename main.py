241fa1624b4ab0d1d5f313ea9e73af6a3a0fdb7f
dukim <sajacall82@naver.com>
2024-05-15 09:47:30 +0900

    :sparkles: 기본 연동 성공

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+import os
 import streamlit as st
+from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate
+from langserve import RemoteRunnable
 from utils import print_messages
 from langchain_core.messages import ChatMessage
 
 
-llm = RemoteRunnable(LANGSERVE_ENDPOINT)
+load_dotenv()
+llm = RemoteRunnable(os.getenv('LANGSERVE_ENDPOINT'))
 
 st.set_page_config(page_title="DaconInfinityGPT", page_icon="🔗")
 st.title("DaconInfinityGPT")
@@ -18,7 +23,15 @@ if user_input := st.chat_input("메시지를 입력해 주세요."):
   st.chat_message("user").write(f"{user_input}")
   st.session_state["messages"].append(ChatMessage(role="user", content=user_input))
 
+  prompt = ChatPromptTemplate.from_template(
+    """질문에 대하여 간결하게 답변해 주세요.
+    {question}
+    """
+  )
+  chain = prompt | llm
+  response = chain.invoke({"question": user_input})
   with st.chat_message("assistant"):
-    msg = f"당신이 입력한 내용은 {user_input}"
+    msg = response.content
+
     st.write(msg)
     st.session_state["messages"].append(ChatMessage(role="assistant", content=msg))
